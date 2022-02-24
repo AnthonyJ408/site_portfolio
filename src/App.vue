3e383566fb6823f7d105782d7851bbde6b69d101
AnthonyJ408 <anthonyy17@live.fr>
2022-02-24 13:28:59 +0100

    Switch Vue2

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <h1>Hello AJ</h1>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-//   },
-// };
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
 <style>
